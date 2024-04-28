c8581c80f85834f632dd209149f1163b7d2fbf7f
bloanfa <2991526787@qq.com>
2024-04-28 19:55:35 +0800

    project1b submission1

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,5 +1,9 @@
-public class OffByN (int N) {
-   
+public class OffByN implements CharacterComparator {
+    private int N;
+    public OffByN(int N) {
+        this.N = N;
+    }
+    @Override
     public boolean equalChars(char x, char y) {
         return Math.abs(x - y) == N;
     }
