2e013b4174fa9d1a1fb8b77915f581ce99c72800
bloanfa <2991526787@qq.com>
2024-05-13 21:31:34 +0800

    hw2 submission1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -4,12 +4,12 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
 //    @override
-    public int capacity(){
+    public int capacity() {
         return capacity;
     }
 
 //    @override
-    public int fillCount(){
+    public int fillCount() {
         return fillCount;
     }
 
